018327df18dc514394ec4068905f0ecbcd22baf5
Miwka <97275239+albertu4@users.noreply.github.com>
2022-06-01 21:03:57 +0900

    Initial Commit

---
+++ SliderValueSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderValueSwiftUI
+//
+//  Created by Михаил Иванов on 01.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SliderValueSwiftUI/SliderValueSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderValueSwiftUIApp.swift
+//  SliderValueSwiftUI
+//
+//  Created by Михаил Иванов on 01.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderValueSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
